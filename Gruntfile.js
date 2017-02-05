c761e15178cd1bbde741ce60ec0d77539f3a2afb
Christopher Hallahan <challahan@gmail.com>
2017-02-04 21:11:56 -0500

    Add critical css

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt) {
           style: 'compressed'
         },
         files: {
-        'assets/css/style.css': 'assets/sass/style.scss'
+        'assets/css/style.css': 'assets/sass/style.scss',
+        'assets/css/critical.css': 'assets/css/critical.css'
         }
       }
     },
